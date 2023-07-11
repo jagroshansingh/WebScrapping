7a99c6b85a8609d70d31c655ae68b160ba5f5214
Jagroshan <jagroshansingh615@gmail.com>
2023-07-11 17:19:16 +0530

    tried creating a pdf

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -15,12 +15,27 @@ const webScrapping= async()=>{
 */
 
 //------------------for listing out all the product name-------------------
+/*
 const webScrapping=async()=>{
     const browser=await puppeteer.launch({headless:'new'})
     const page=await browser.newPage()
     await page.goto('https://pptr.dev/api/puppeteer.page.screenshot')
     const sideList=await page.evaluate(()=>Array.from(document.querySelectorAll('.menu__link')).map((el)=>el.textContent))
     fs.writeFile('list.txt',sideList.join("\n"))
+
+    await browser.close()
+}
+*/
+//
+
+//-------------------------for creating a PDF-----------------------------
+const webScrapping=async()=>{
+    const browser=await puppeteer.launch({headless:'new'})
+    const page=await browser.newPage()
+    await page.goto('https://pptr.dev/api/puppeteer.page.pdf')
+    // await page.emulateMediaType('screen')
+    // await page.evaluate(()=>matchMedia('screen').matches)
+    await page.pdf({path:'converting.pdf'})
 
     await browser.close()
 }
