aaecd641ae0018daceef1519a556ab7ccd025f65
Jagroshan <jagroshansingh615@gmail.com>
2023-07-18 22:31:00 +0530

    scrapping bookData from amazon.com

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -48,17 +48,64 @@ const webScrapping=async()=>{
     const page=await browser.newPage()
     await page.goto('https://unsplash.com/')
     // let img=await page.evaluate(()=>Array.from(document.querySelectorAll('.MorZF img')).map((el)=>el.alt))
-    // console.log(img)
-    let images=await page.$$eval('.MorZF img',el=>el.map((image)=>image.src))
-    let altText=await page.$$eval('.MorZF img',el=>el.map((image)=>image.alt))
+    let img=await page.waitForSelector('#app > div > div.b4j4s > div > div > div > div.BK8tG > div > div.pRk2s > div.VCR4P > ul > li:nth-child(5) > a')
+    console.log(img)
+    // let images=await page.$$eval('.MorZF img',el=>el.map((image)=>image.src))
+    // let altText=await page.$$eval('.MorZF img',el=>el.map((image)=>image.alt))
 
-    for(let i=0; i<images.length; i++)
-    {
-        let picture=await page.goto(images[i])
-        fs.writeFile(`./pics/${altText[i]}.jpeg`,await picture.buffer())
-    }
+    // for(let i=0; i<images.length; i++)
+    // {
+    //     let picture=await page.goto(images[i])
+    //     fs.writeFile(`./pics/${altText[i]}.jpeg`,await picture.buffer())
+    // }
 
     await browser.close()
 }
 
-webScrapping();
+// webScrapping();
+
+//----------------------scrapping the bookData from amazon------------------
+
+async function scrapeBookData() {
+    const browser = await puppeteer.launch({headless:'new'});
+    const page = await browser.newPage();
+  
+    // Set up a navigation listener to wait until the content is fully loaded
+
+    await page.goto('https://www.amazon.com/best-sellers-books-Amazon/zgbs/books', { waitUntil: 'domcontentloaded' });
+
+    try {
+        await page.waitForSelector('.p13n-gridRow', { timeout: 8000 });
+    } catch (error) {
+        console.error('Timeout waiting for book data to load.');
+        await browser.close();
+        return;
+    }
+
+    // Scrape book data
+    const bookData = await page.evaluate(() => {
+      const books = Array.from(document.querySelectorAll('.p13n-gridRow > div[id^="gridItemRoot"]'));
+  
+      return books.map((book) => {
+        const titleElement = book.querySelector('a[class="a-link-normal"]');
+        const authorElement = book.querySelector('a[class="a-size-small a-link-child"]');
+        const formatElement = book.querySelector('span[class="a-size-small a-color-secondary a-text-normal"]');
+        const priceElement = book.querySelector('span[class="a-size-base a-color-price"]');
+
+        const title = titleElement ? titleElement.innerText.trim() : '';
+        const author = authorElement ? authorElement.innerText.trim() : '';
+        const format = formatElement ? formatElement.innerText.trim() : '';
+        const price = priceElement ? priceElement.innerText.trim() : '';
+
+        return { title, author, format, price };
+      });
+    });
+  
+    fs.writeFile('bookData.json', JSON.stringify(bookData, null, 2));
+  
+    await browser.close();
+  }
+  
+  scrapeBookData();
+
+  
